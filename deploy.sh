b3ddcaf11e57ce9927ac64cfb12db77d7544c7df
Kimberly Grey <kimandthemachine@gmail.com>
2020-05-21 15:28:53 +0100

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+rsync -av \
+      --exclude 'bin/' \
+      --exclude 'components/' \
+      --exclude 'docs/' \
+      --exclude 'gulpfiles/' \
+      --exclude 'node_modules/' \
+      --exclude 'src/' \
+      * \
+      querkmachine@berly.kim:webapps/severnbronies_2020/wp-content/themes/plimsoll/
