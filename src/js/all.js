f2a25ba557a6ec8336b5738cb6d956fc734a6381
Kimberly Grey <kimandthemachine@gmail.com>
2020-05-22 16:37:02 +0100

    Add boilerplate banner

--- src/js/all.js
+++ src/js/all.js
@@ -4,35 +4,30 @@
  * class on that component. It will only do this if the component is explicitly
  * listed here, and only if it finds the component on the page, however it
  * loads the JS for *ALL* components, as they're all imported at once.
- * 
+ *
  * You can keep JS filesizes small by creating different entry files that only
  * include the components needed for a page or section.
- * 
+ *
  * This file also exports all component classes so that they can be initialized
  * individually, if necessary.
  *
- * To initialise everything at once: `window.sb.initAll()`
+ * To initialise everything in this file at once: `window.sb.initAll()`
+ */
 
-import Datepicker from '../../components/datepicker/datepicker';
+import "what-input";
+
+import Banner from "../../components/banner/banner";
 
 function initAll(options) {
-	options = typeof options !== 'undefined' ? options: {};
+	options = typeof options !== "undefined" ? options : {};
 
 	// Scope initialization to only certain parts of the page
 	// Defaults to entire document if not set
-	const scope = typeof options.scope !== 'undefined' ? options.scope : document;
+	const scope = typeof options.scope !== "undefined" ? options.scope : document;
 
-	scope.querySelectorAll('[data-module="kickstart-datepicker"]').forEach(datepicker => {
-		new Datepicker(datepicker);
+	scope.querySelectorAll('[data-module="sb-banner"]').forEach(m => {
+		new Banner(m);
 	});
-
 }
 
-export {
-	initAll,
-	Datepicker
-}
-
-*/
-
-import "what-input";
+export { initAll, Banner };
