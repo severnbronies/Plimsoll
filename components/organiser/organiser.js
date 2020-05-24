8384f5fd3c9fd9d0c84455b58d07331cb0dc68ec
Kimberly Grey <kimandthemachine@gmail.com>
2020-05-24 13:10:10 +0100

    Add meet organiser component.

---
+++ components/organiser/organiser.js
@@ -0,0 +1,6 @@
+export default class Organiser {
+	constructor($module) {
+		this.$module = $module;
+		// Put code here
+	}
+}
--- single.php
+++ single.php
@@ -2,6 +2,17 @@
 
 $context = Timber::context();
 $timber_post = Timber::query_post();
+
 $context['post'] = $timber_post;
+$context['location'] = sb_location_twig_function($timber_post->ID);
+
+$runners = sb_runner_twig_function($timber_post->ID);
+$context['runners'] = $runners;
+
+$runners_names = [];
+foreach ($runners as $runner) {
+	$runners_names[] = $runner['name'];
+}
+$context['runners_names'] = $runners_names;
 
 Timber::render('meet.twig', $context);
