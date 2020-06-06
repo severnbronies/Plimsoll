257f574a7bfa488ef7d6f6d5a3fbf455c1e89337
Kimberly Grey <kimandthemachine@gmail.com>
2020-06-06 11:26:15 +0100

    Display categories on meet page.

--- single.php
+++ single.php
@@ -10,11 +10,18 @@ $context['locations'] = sb_location_twig_function($timber_post->ID);
 
 $runners = sb_runner_twig_function($timber_post->ID);
 $context['runners'] = $runners;
-
 $runners_names = [];
 foreach ($runners as $runner) {
 	$runners_names[] = $runner['name'];
 }
 $context['runners_names'] = $runners_names;
+
+$categories = get_the_category();
+$context['categories'] = $categories;
+$categories_names = [];
+foreach ($categories as $category) {
+	$categories_names[] = $category->name;
+}
+$context['categories_names'] = $categories_names;
 
 Timber::render('meet.twig', $context);
