0d86408d39e7983398c72cb900227727e82aa3bf
Kimberly Grey <kimandthemachine@gmail.com>
2020-09-12 17:38:57 +0100

    Replace Fitty with simpler bespoke solution.

--- components/meet-date/meet-date.js
+++ components/meet-date/meet-date.js
@@ -1,16 +1,33 @@
-import Fitty from "fitty";
-
 export default class MeetDate {
 	constructor($module) {
 		this.$module = $module;
-		// this.$module.childNodes.forEach((child) => {
-		// 	console.log(child);
-		// 	Fitty(child);
-		// });
-		for (let child of this.$module.childNodes) {
-			if (child.nodeType !== Node.TEXT_NODE) {
-				Fitty(child);
-			}
+		this.nodes = this.nodeList();
+		this.bindEvents();
+	}
+	nodeList() {
+		return [...this.$module.childNodes].filter(
+			node => node.nodeType !== Node.TEXT_NODE
+		);
+	}
+	bindEvents() {
+		window.addEventListener("load", this.reflowAll.bind(this));
+		window.addEventListener("resize", this.reflowAll.bind(this));
+		window.addEventListener("orientationchange", this.reflowAll.bind(this));
+	}
+	reflowAll() {
+		this.nodes.forEach(el => this.resetNode(el));
+		this.containerWidth = this.$module.getBoundingClientRect().width;
+		this.nodes.forEach(el => this.sizeNode(el));
+	}
+	resetNode($node) {
+		$node.removeAttribute("style");
+	}
+	sizeNode($node) {
+		if ($node.fitInitialWidth) {
+			const initialWidth = $node.fitInitialWidth;
+		} else {
+			$node.fitInitialWidth = $node.getBoundingClientRect().width;
 		}
+		$node.style.fontSize = `${this.containerWidth / $node.fitInitialWidth}em`;
 	}
 }
