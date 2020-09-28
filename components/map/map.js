2b6d7de2f9cce5fbc45598ad4648c8f79825059d
Kimberly Grey <kimandthemachine@gmail.com>
2020-09-28 22:41:33 +0100

    Update Google Maps code to use some beta features.
    This includes WebGL-based vector maps and cloud-based styling. https://cloud.google.com/blog/products/maps-platform/introducing-new-maps-customization-features

--- components/map/map.js
+++ components/map/map.js
@@ -4,6 +4,10 @@ export default class Map {
 		this.$map = $module.querySelector(".sb-map__map");
 		this.$locationList = $module.querySelector(".sb-map__list");
 		this.apiKey = apiKey;
+		this.mapStyleIds = {
+			light: "52763fe8da2feb10",
+			dark: "658043d2c8977d9b"
+		};
 		this.markers = [];
 		this.cb = cb;
 		this.loadGoogleMaps();
@@ -11,7 +15,9 @@ export default class Map {
 	loadGoogleMaps() {
 		// Create the script tag, set the appropriate attributes
 		var script = document.createElement("script");
-		script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}`;
+		script.src = `https://maps.googleapis.com/maps/api/js?v=beta&key=${
+			this.apiKey
+		}&map_ids=${Object.values(this.mapStyleIds).join()}`;
 		script.defer = true;
 		script.async = true;
 		script.onload = () => {
@@ -24,7 +30,9 @@ export default class Map {
 			center: { lat: 0, lng: 0 },
 			zoom: 0,
 			gestureHandling: "cooperative",
-			styles: this.mapStyle(),
+			mapId: window.matchMedia("(prefers-color-scheme: dark)").matches
+				? this.mapStyleIds.dark
+				: this.mapStyleIds.light,
 			disableDefaultUI: true
 		});
 		this.map.addListener("idle", () => {
@@ -89,175 +97,5 @@ export default class Map {
 	repaintMapBounds() {
 		this.map.fitBounds(this.mapBoundary);
 		this.map.setCenter(this.mapBoundary.getCenter());
-	}
-	mapStyle() {
-		if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-			return [
-				{ elementType: "geometry", stylers: [{ color: "#212121" }] },
-				{ elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-				{ elementType: "labels.text.fill", stylers: [{ color: "#757575" }] },
-				{ elementType: "labels.text.stroke", stylers: [{ color: "#212121" }] },
-				{
-					featureType: "administrative",
-					elementType: "geometry",
-					stylers: [{ color: "#757575" }]
-				},
-				{
-					featureType: "administrative.country",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#9e9e9e" }]
-				},
-				{
-					featureType: "administrative.land_parcel",
-					stylers: [{ visibility: "off" }]
-				},
-				{
-					featureType: "administrative.locality",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#bdbdbd" }]
-				},
-				{
-					featureType: "poi",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#757575" }]
-				},
-				{
-					featureType: "poi.park",
-					elementType: "geometry",
-					stylers: [{ color: "#181818" }]
-				},
-				{
-					featureType: "poi.park",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#616161" }]
-				},
-				{
-					featureType: "poi.park",
-					elementType: "labels.text.stroke",
-					stylers: [{ color: "#1b1b1b" }]
-				},
-				{
-					featureType: "road",
-					elementType: "geometry.fill",
-					stylers: [{ color: "#2c2c2c" }]
-				},
-				{
-					featureType: "road",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#8a8a8a" }]
-				},
-				{
-					featureType: "road.arterial",
-					elementType: "geometry",
-					stylers: [{ color: "#373737" }]
-				},
-				{
-					featureType: "road.highway",
-					elementType: "geometry",
-					stylers: [{ color: "#3c3c3c" }]
-				},
-				{
-					featureType: "road.highway.controlled_access",
-					elementType: "geometry",
-					stylers: [{ color: "#4e4e4e" }]
-				},
-				{
-					featureType: "road.local",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#616161" }]
-				},
-				{
-					featureType: "transit",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#757575" }]
-				},
-				{
-					featureType: "water",
-					elementType: "geometry",
-					stylers: [{ color: "#000000" }]
-				},
-				{
-					featureType: "water",
-					elementType: "labels.text.fill",
-					stylers: [{ color: "#3d3d3d" }]
-				}
-			];
-		}
-		return [
-			{ elementType: "geometry", stylers: [{ color: "#f5f5f5" }] },
-			{ elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-			{ elementType: "labels.text.fill", stylers: [{ color: "#616161" }] },
-			{ elementType: "labels.text.stroke", stylers: [{ color: "#f5f5f5" }] },
-			{
-				featureType: "administrative.land_parcel",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#bdbdbd" }]
-			},
-			{
-				featureType: "poi",
-				elementType: "geometry",
-				stylers: [{ color: "#eeeeee" }]
-			},
-			{
-				featureType: "poi",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#757575" }]
-			},
-			{
-				featureType: "poi.park",
-				elementType: "geometry",
-				stylers: [{ color: "#e5e5e5" }]
-			},
-			{
-				featureType: "poi.park",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#9e9e9e" }]
-			},
-			{
-				featureType: "road",
-				elementType: "geometry",
-				stylers: [{ color: "#ffffff" }]
-			},
-			{
-				featureType: "road.arterial",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#757575" }]
-			},
-			{
-				featureType: "road.highway",
-				elementType: "geometry",
-				stylers: [{ color: "#dadada" }]
-			},
-			{
-				featureType: "road.highway",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#616161" }]
-			},
-			{
-				featureType: "road.local",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#9e9e9e" }]
-			},
-			{
-				featureType: "transit.line",
-				elementType: "geometry",
-				stylers: [{ color: "#e5e5e5" }]
-			},
-			{
-				featureType: "transit.station",
-				elementType: "geometry",
-				stylers: [{ color: "#eeeeee" }]
-			},
-			{
-				featureType: "water",
-				elementType: "geometry",
-				stylers: [{ color: "#c9c9c9" }]
-			},
-			{
-				featureType: "water",
-				elementType: "labels.text.fill",
-				stylers: [{ color: "#9e9e9e" }]
-			}
-		];
 	}
 }
