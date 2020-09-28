2b59a7ba935522457c766c45a37c796901619a6f
Kimberly Grey <kimandthemachine@gmail.com>
2020-09-28 23:13:25 +0100

    Remove meet archive nav fanciness. Keep it simple, yo.

--- components/archive-navigation/archive-navigation.js
+++ components/archive-navigation/archive-navigation.js
@@ -1,18 +1,6 @@
 export default class ArchiveNavigation {
 	constructor($module) {
 		this.$module = $module;
-		this.$list = $module.querySelector(".sb-archive-navigation__list");
-		this.rememberScrollPos();
-	}
-	rememberScrollPos() {
-		const sessionId = "archive-nav-scroll-pos";
-		const top = sessionStorage.getItem(sessionId);
-		if (top !== null) {
-			this.$list.scrollTop = parseInt(top, 10);
-		}
-		window.addEventListener("beforeunload", () => {
-			sessionStorage.setItem(sessionId, this.$list.scrollTop);
-			console.log(this.$list.scrollTop);
-		});
+		// Put code here
 	}
 }
